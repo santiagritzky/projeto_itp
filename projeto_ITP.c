2ca84a44c867b6d00d47da7f5e9d9a8a5c1c8882
Guilherme Santiago <98118165+santiagritzky@users.noreply.github.com>
2022-12-26 15:36:34 -0300

    Update projeto_ITP.c

--- projeto_ITP.c
+++ projeto_ITP.c
@@ -1,140 +1,219 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
-// Constantes para as strings a serem lidas 
-#define MAX_LINE 100
-#define MAX_ACTION 10
 #define MAX_ID_SIZE 10
+#define HAND_SIZE 7
+#define MAX_ID_SIZE 10
+#define MAX_LINE 100
 
-void debug(char *message) {
-  fprintf(stderr, "%s\n", message);
+char *naipe[] = {"♥", "♦", "♣", "♠"};
+char *valor[] = {"A", "2", "3",  "4", "5", "6", "7",
+                 "8", "9", "10", "V", "D", "R", "C"};
+
+typedef struct {
+  char naipe[5];
+  char valor[3];
+} carta;
+
+//**
+carta ler(char *currentCard) {
+  carta carta;
+  strcpy(carta.valor, &currentCard[0]);
+  strcpy(carta.naipe, &currentCard[1]);
+  return carta;
+}
+
+//separa a string de jogadores e conta quantos são
+char **read_players(char *input) {
+  char *split, **players;
+  int count = 0;
+
+  split = strtok(input, " ");
+  while (split != NULL) {
+    count++;
+    split = strtok(NULL, " ");
+  }
+  players = malloc(sizeof(char *) * count);
+  split = strtok(input, " ");
+  for (int i = 0; i < count; i++) {
+    players[i] = malloc(sizeof(char) * strlen(split));
+    strcpy(players[i], split);
+    split = strtok(NULL, " ");
+  }
+  return players;
+}
+
+//trata os [ ] e separa a mão em cartas
+carta *read_hand(char input[]) {
+  carta *hand;
+  char *split, **cards;
+  int count = 0;
+
+  input[0] = ' ';
+  input[strlen(input - 1)] = ' ';
+
+  split = strtok(input, " ");
+  while (split != NULL) {
+    count++;
+    split = strtok(NULL, " ");
+  }
+  cards = malloc(sizeof(char) * count);
+  split = strtok(input, " ");
+  for (int i = 0; i < count; i++) {
+    cards[i] = malloc(sizeof(char) * strlen(split));
+    strcpy(cards[i], split);
+    split = strtok(NULL, " ");
+  }
+
+  hand = malloc(sizeof(carta) * count);
+  for (int i = 0; i > count; i++) {
+    hand[i].valor[0] = cards[i][0];
+    hand[i].valor[1] = '\0';
+    hand[i].naipe[0] = cards[i][1];
+    hand[i].naipe[1] = '\0';
+  }
+  return hand;
+}
+
+//adiciona carta nova à mão
+carta* comprar(carta* hand, int* handSize, char* card) {
+  hand = realloc(hand, sizeof(carta) * (*handSize + 1));
+  (*handSize)++;
+  hand[*handSize] = ler(card);
+}
+
+//descarta, diminui handSize
+carta* descartar(int descarte, carta *hand, int *handSize) {
+
+  for (int i = descarte; i < *handSize - 1; i++) {
+    hand[i] = hand[i + 1];
+  }
+  (*handSize)--;
+  hand = realloc(hand, sizeof(carta) * *handSize);
+  return hand;
 }
 
 int main() {
- 
-  char temp[MAX_LINE];      // string para leitura temporária de dados NECESSARIO TRATAR ESSA VARIAVEL PARA DIMINUIR O NUMERO VARIAVEIS UTILIZADAS
-  char my_id[MAX_ID_SIZE];  // identificador do seu bot
-  char reading[MAX_LINE];   // string para leitura das cartas criada pela falta de tratamento de temp, futuramente vai ser excluida
-  char HAND[MAX_ACTION][MAX_LINE]; // string para guardar as cartas recebidas declarada estaticamente mais deve ser alocada dinamicamente por conta das cartas a ser compradas e descartadas durante a partida
 
-  setbuf(stdin, NULL);   // stdin, stdout e stderr não terão buffers
-  setbuf(stdout, NULL);  // assim, nada é "guardado temporariamente"
+  char table[10];
+  char *id;
+  char startingHand[MAX_LINE];
+  char temp[MAX_LINE];
+  char action[MAX_LINE];
+  char complement[MAX_LINE];
+  char scndComplement[MAX_LINE];
+  char *arrayPlayers;
+  int comprada = 0;
+
+  setbuf(stdin, NULL);
+  setbuf(stdout, NULL);
   setbuf(stderr, NULL);
 
-  // === INÍCIO DA PARTIDA ===
+  scanf("PLAYERS %[^\n]\n", arrayPlayers);
+  char **players = read_players(arrayPlayers);
 
-  /*Um exemplo de dados iniciais é:
-    PLAYERS b1 b2 b3
-    YOU b1
-    HAND [ 4♥ 7♦ 2♣ V♠ A♥ 3♦ 2♣ 9♠ ]
-    TABLE 8♦*/
+  scanf("YOU %s\n", id);
+  char *my_id = malloc(sizeof(char) * strlen(id) + 1);
+  strcpy(my_id, id);
 
-  scanf("PLAYERS %[^\n]\n", temp);
+  scanf("HAND [ %[^\n] ]\n", startingHand);
+  carta *hand = read_hand(startingHand);
 
-  debug(temp);
+  scanf("TABLE %s\n", table);
+  carta mesa;
+  mesa.valor[0] = table[0];
+  mesa.valor[1] = '\0';
+  mesa.naipe[0] = table[1];
+  mesa.naipe[1] = '\0';
 
-  // Lê o identificador do próprio bot. Isso é importante para testar quando é sua vez.
-  scanf("YOU %s\n", my_id);
-  
-  scanf("HAND %[^\n]\n", reading); // Leitura da 'mão' do jogo
-
-  int count = 0;
-    for(int i = 0; i <= sizeof(reading); i++){
-        if(reading[i] != '[' && reading[i] != ' ' && reading[i] != ']'){
-           reading[count] = reading[i];
-            count++;
-        }
-    }                                   //Utilizado para tratar a entrada de HAND mas só esta funcionando parcialmente e é necessario utilizar isso em função para deixar o codigo mais limpo
-    for (int i = 0; i <= 8; i++){
-        for (int j = 0; j <= 4; j++){
-            HAND[i][j] = reading[j];
-        }
-    }
-
-  //Lê a carta aberta sobre a mesa. Ex: TABLE 8♣
-  scanf("TABLE %s\n", temp);
-
-  printf("%s\n", reading);
-  printf("%s \n", HAND[8]);
-
-  // === PARTIDA ===
-
-  /char id[MAX_ID_SIZE];
-  char action[MAX_ACTION];
-  char complement[MAX_LINE];
-  char card[];
-  int acao;
-
- 
-  while(1) {
+  while (1) {
+    // turno
     do {
-      
-
       scanf("%s %s", action, complement);
-
-      if (strcmp(action, "DISCARD")){ // Testes de caso de 'DISCARD' 
-        for (int i = 0; i <= 8; i++){
-            for (int j = 0; j <= 5; j++){
-                if (strcmp(complement, HAND[i])){
-                    char card = HAND[i];     // Não funcional, apenas uma ideia de logica 
-                }
-            }   
-        }
-        
-        
-        if (strcmp(complement[0], 'C')){
-            acao = 1;
-        }
-        if (strcmp(complement[0], 'V')){
-            acao = 2;
-        }
-        /*if (strcmp(complement[0], 'D')){   
-            acao = 3;
-        }
-        if (strcmp(complement[0], 'R')){    // necessario realizar a leitura mas não sei se precisa realizar alguma ação ou isso é de responsabilidade do gerenciador do jogo.
-            acao = 4;
-        }
-        if (strcmp(complement[0], 'A')){
-            acao = 5;
-        }*/
+      if (strcmp(action, "SAY") == 0) {
+        /* handSize >= 5 ? printf("%s", desculpinha) : printf("%s", pabulosidade)
+        */
       }
-      
-
-      
-
+      if (strcmp(action, "DISCARD") == 0) {
+        if (complement[0] == 'A') {
+          scanf(" %s", scndComplement);
+          for (int i = 1; i < 4; i++) {
+            complement[i] = scndComplement[i - 1];
+          }
+        }
+        if (complement[0] == 'C' || complement[0] == 'V') {
+          comprada = 0;
+          mesa = ler(complement);
+        }
+      } else if (strcmp(action, "BUY") == 0) {
+          comprada = 1;
+      }        
     } while (strcmp(action, "TURN") || strcmp(complement, my_id));
-    
-    // agora é a vez do seu bot jogar
-    debug("----- MINHA VEZ -----");
-    
-    switch (acao){
-    case 1: 
-        printf("BUY 4"); 
-        //scanf(%[^\n]\n, HAND);
-        // inserir função para adicionar nova carta
-        //scanf(%[^\n]\n, HAND);
-        // inserir função para adicionar nova carta
-        //scanf(%[^\n]\n, HAND);
-        // inserir função para adicionar nova carta
-        //scanf(%[^\n]\n, HAND);
-        // inserir função para adicionar nova carta
-        break;     
-    case 2:     // Case 1 e 2, necessario implementar a alocação dinamica na variavel HAND e alocar as novas cartas compradas
-        printf("BUY 2");
-        //scanf(%[^\n]\n, HAND);
-        // inserir função para adicionar nova carta
-        //scanf(%[^\n]\n, HAND);
-        // inserir função para adicionar nova carta
-        break;
-        scanf();
-    default:
-        break;
+    if (strcmp(mesa.valor, "V") == 0 && comprada != 1) {
+      printf("BUY 2\n");
+      comprada = 1;
+      for (int i = 0; i < 2; i++) {
+        scanf(" %s", temp);
+        hand = comprar(hand, &handSize, temp);
+      }
+    } else if (mesa.valor == "C" && comprada != 1) {
+      printf("BUY 4\n");
+      comprada = 1;
+      for (int i = 0; i < 4; i++) {
+        scanf(" %s", temp);
+        hand = comprar(hand, &handSize, temp);
+      }
+    } else {
+      int existe = 0; //default
+      int coringa = 0;
+      int place; //index da carta
+      for (int i = 0; i < handSize; i++) {
+        if(mesa.valor == hand[i].valor || mesa.naipe == hand[i].naipe) {
+          existe = 1;
+          place = i;
+          break;
+        } else if (hand[i].valor == "C") {
+          coringa = 1;
+          place = i;
+        }
+      }
+      //quebrar isso em funcoes
+      if (existe == 1) {
+        /* se bot tem uma carta jogável e ela for 
+        especial, escolhe o naipe mais frequente na mão: */
+        if (hand[place].valor == "A"|| hand[place].valor == "C") {
+          int copas = 0;
+          int espadas = 0;
+          int ouro = 0;
+          int paus = 0;
+          for (int i = 0; i < handSize; i++){
+            if (strcmp(hand[place].naipe, naipe[0]) == 0) { copas++; }
+            if (strcmp(hand[place].naipe, naipe[1]) == 0) { ouro++; }
+            if (strcmp(hand[place].naipe, naipe[2]) == 0) { paus++; }
+            if (strcmp(hand[place].naipe, naipe[3]) == 0) { espadas++; }
+          }
+          int suits[] = {copas, espadas, ouro, paus};
+          int maior = 0;
+          for (int i = 0; i < 4; i++) {
+            if (suits[i] > maior) {
+               maior = suits[i];
+            }
+          }
+          //C ou V
+          printf("DISCARD %s%s %s\n", hand[place].valor, hand[place].naipe, suits[maior]);
+        } else {
+          //se não for especial ele só descarta sem scndComplement
+          printf("DISCARD %s%s\n", hand[place].valor, hand[place].naipe);
+        }
+        int descarte = place;
+        hand = descartar(descarte, hand, handSize);
+      //se não houver carta compatível ele compra uma
+      } else {
+        printf("BUY 1\n");
+        scanf(" %s", temp);
+        hand = comprar(hand, handSize, temp);
+      }    
     }
-
-    printf("DISCARD %s\n", card);
-    //Necessario implementar uma função para retiar a carta que foi descartada
   }
-
-  return 0;*/
 }
